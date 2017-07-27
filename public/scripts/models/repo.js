f847b16b86b3392086b600a0483d50406a4f908c
Chaitanya.Narukulla <Chai@Ashleys-MacBook-Air.local>
2017-07-27 09:41:41 -0700

    Publishing

--- public/scripts/models/repo.js
+++ public/scripts/models/repo.js
@@ -11,13 +11,14 @@ var app = app || {};
     //       Don't forget to remove the headers from our request - we're no longer using a token on the
     //       client side of our app, our new proxyGitHub function will be handling the token using our
     //       new environment variable!
-    $.ajax({
-      url: `https://api.github.com/user/repos`,
-      type: 'GET',
-      headers: {'Authorization': `token ${githubToken}`}
-    })
-    .then(data => repos.all = data, err => console.error(err)) // es6 syntax arrow functions
-    .then(callback);
+    // $.ajax({
+    //   url: `https://api.github.com/user/repos`,
+    //   type: 'GET',
+    //   headers: {'Authorization': `token ${githubToken}`}
+    // })
+    $.get('/github/users/repos')
+      .then(data => repos.all = data, err => console.error(err)) // es6 syntax arrow functions
+      .then(callback);
   };
 
   repos.with = attr => repos.all.filter(repo => repo[attr]);
